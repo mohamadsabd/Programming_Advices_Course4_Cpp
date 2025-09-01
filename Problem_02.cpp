0735f35eeb5c9088ef7953bb4292b253754b0920
Mohammad Abd_Alkareem <mohamad.2016.abd@gmail.com>
2025-09-01 17:32:49 +0300

    Solve Problem 2 : Read and Print Name

---
+++ Problem_02.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string ReadName()
+{
+    string name = "";
+    cout << "Please Enter The Name : ";
+    getline(cin, name);
+    return name;
+}
+
+void PrintName(string name)
+{
+    cout << "\nYour Name is : " << name << "\n";
+}
+
+int main()
+{
+    PrintName(ReadName());
+    return 0;
+}
